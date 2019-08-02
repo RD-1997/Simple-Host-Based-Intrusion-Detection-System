fc3b9bbfcd43e0cae59860edd5b2776dc52409f5
Hisk58 <47060156+Hisk58@users.noreply.github.com>
2019-08-02 03:04:19 +0200

    Final update

--- main.c
+++ main.c
@@ -15,59 +15,50 @@
 #pragma comment(lib, "ws2_32.lib") //library winsock
 
 /* ####################################
- * Refers to the sub functions
+ * Refers to the functions
  * ####################################*/
 
-void giveUnnecessaryMessage();
-int countDown(int);
-void list_files();
-void start_winsock();
-void create_socket();
-void socket_send(char *);
+void giveUnnecessaryMessage(); // Function: unnecessary loop
+int countDown(int); // Function: Use countdown for loop
+void list_files(); // Function: make array list
+void list_filesz(); // Function: duplicate array of file names for hashing.
+void start_winsock(); // Function: start winsock to use socket on Windows OS.
+void create_socket(); // Function: create socket / connect to server
+void socket_send(char *); // Function: send socket with message.
+void encrypt_data(char *); // Function: encrypt some string with caesar cipher algorithm.
 
-
-/* global variables */
-char directory[] = {"C:/Users/test/Downloads/test/"};
-int counter, i = 0, j = 0, k = 0, l = 254, n = 0;
+/* ####################################
+ * Global Variables
+ * ####################################*/
+char directory[] = {"C:/Users/test/Downloads/test/"}; // directory path..
+int counter, i = 0, ii = 0; // some variables used for loops...
 char *filesList[254];// gives error when you use variables
-SOCKET s;
-struct sockaddr_in server;
-
-char *makeonstring(size_t size, char *array[size], const char *joint){
-    size_t jlen, lens[size];
-    size_t i, total_size = (size-1) * (jlen=strlen(joint)) + 1;
-    char *result, *p;
-
-
-    for(i=0;i<size;++i){
-        total_size += (lens[i]=strlen(array[i]));
-    }
-    p = result = malloc(total_size);
-    for(i=0;i<size;++i){
-        memcpy(p, array[i], lens[i]);
-        p += lens[i];
-        if(i<size-1){
-            memcpy(p, joint, jlen);
-            p += jlen;
-        }
-    }
-    *p = '\0';
-    return result;
-}
+char *filesList2[254]; // second array
+char temp[254]; // 3th array
+SOCKET s; // global variable for socket
+struct sockaddr_in server; //global variable for socket
 
 int main() {
     giveUnnecessaryMessage();
-    start_winsock();
-    create_socket();
-    printf("Enter in seconds a value ot repeat the program\n");
+    start_winsock(); // start winsock
+    create_socket(); // create the socket
+    printf("Enter in seconds a value ot repeat the program\n"); // ask for countdown, als usable for loop.
     scanf("%d",&counter); // scan the answer and put in counter
-    //loop
     countDown(counter); // use answer counter in function
-    list_files(); //update array
-    char *cat = makeonstring(i, filesList, ";");
-    puts(cat);
-    socket_send(cat);
+    list_files(); // fill in one array
+    list_filesz(); // fill in second array for manipulation (hashing)
 
+    for(int z = 0; z<i; z++){
+        char *tempx = filesList[z]; //use temp as single string for function;
+        encrypt_data(tempx); // encrypt string in temp (each line in array)
+    }
+    for (int x = 0; x<i;x++){
+        strcat(filesList[x], ";"); // add ; to seperate hash and filename
+        snprintf(temp, 254, "%s%s", filesList[x], filesList2[x]); // add filename and hash together.
+        socket_send(temp); // send through socket inside loop..
+        create_socket(); // reconnect to send again..
+        sleep(2); // sleep 2 seconds.
+    }
     return 0;
 }
 /* function for counting down */
@@ -96,12 +87,23 @@ void list_files()
             i++; // do +1 to read each single array line
         }
     }
-//    for(n = 0; n < i;n++){
-//        printf("%s\n", filesList[n]);
-//    }
-//    for(n = 0; n < i;n++){
-//        free ( filesList[n]);
-//    }
+
+}
+void list_filesz()
+{
+    DIR *d;
+    struct dirent *dir;
+    d = opendir(directory);
+    //Put file names into the array
+    while((dir = readdir(d)) != NULL) {
+        if ( ( ii < 254) && ! ( strcmp(dir->d_name, ".") == 0 || strcmp(dir->d_name, "..") == 0))
+        {
+            filesList2[ii] = malloc( strlen ( dir->d_name) + 1); // allocate memory
+            strcpy(filesList2[ii], dir->d_name); // put file names in to array
+            ii++; // do +1 to read each single array line
+        }
+    }
+
 }
 void start_winsock()
 {
@@ -118,7 +120,6 @@ void create_socket()
     if ((s= socket(AF_INET, SOCK_STREAM, 0 )) == INVALID_SOCKET)
         printf("Could not create socket: %d", WSAGetLastError);
 
-    printf("\nSocket Has been created successfully..");
     server.sin_addr.s_addr = inet_addr("192.168.2.39");
     server.sin_family = AF_INET;
     server.sin_port = htons(12345);
@@ -128,21 +129,20 @@ void create_socket()
         puts("connect error");
     }
 
-    puts("\nSuccessfully connected to the server..\n");
 }
 void socket_send(char *message) {
 
         if(send(s, message,strlen(message), 0) < 0) {
             puts("Send failed");
         }
-
-
     puts("Data send");
 
 }
-/* ####################################
- * Message displayed when launching program
- * ####################################*/
+void encrypt_data(char *encrypt)
+{
+    for(int z = 0; (z < 100 && encrypt[z] != '\0'); z++)
+        encrypt[z] = encrypt[z] + 3;
+}
 void giveUnnecessaryMessage()
 {
     printf("###########################################\n");
@@ -152,5 +152,3 @@ void giveUnnecessaryMessage()
     printf("##### Raoul Dinmohamed & Hakan Kece #######\n");
     printf("###########################################\n");
 }
-
-
