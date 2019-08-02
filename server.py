fa36bb8864364a749f0c9b1293a0510a627301dd
Hisk58 <47060156+Hisk58@users.noreply.github.com>
2019-08-02 03:04:50 +0200

    Update server.py

--- server.py
+++ server.py
@@ -1,38 +1,57 @@
 import socket
-import time
-from _thread import *
+import sys
+import struct
 
-addr = "192.168.2.62"
-port = 8080
+HOST = ''  # Symbolic name, meaning all available interfaces
+PORT = 12345  # Arbitrary non-privileged port
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-print("Socket succesfully created")
-s.bind((addr, port))
-print("Succesfully bound to, ", (addr, port))
-s.listen(1)
-print("Listening for connection...")
+print('Socket created')
 
+def decrypt(data):
+    shift = 3
+    space = []
 
-def clientthread(conn):
-    welcomeMessage = ("Welcome to the server. Type something and hit enter\n")
-    conn.send(welcomeMessage.encode())
+    ciphertext = data.split()
+    sentence = []
 
-    while True:
-        data = conn.recv(8096)
-        reply = ("Message of the client:" + data.decode())
-        if not data:
-            break;
-        print(reply)
-        conn.sendall(reply.encode())
-    conn.close()
+    for word in ciphertext:
+        cipher_ords = [ord(x) for x in word]
+        plaintext_ords = [o - shift for o in cipher_ords]
+        plaintext_chars = [chr(i) for i in plaintext_ords]
+        plaintext = ''.join(plaintext_chars)
+        sentence.append(plaintext)
 
+    sentence = ' '.join(sentence)
+    print('Decryption Successful\n')
 
-while True:
-    conn, address = s.accept()
-    print(f"Connection from {address} has been established")
-    start_new_thread(clientthread, (conn,))
+    return sentence
+
+# Bind socket to local host and port
+try:
+    s.bind((HOST, PORT))
+
+except socket.error as msg:
+    print('Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1])
+    sys.exit()
+
+print('Socket bind complete')
+
+# Start listening on socket
+s.listen(10)
+print('Socket now listening')
+
+# now keep talking with the client
+while 1:
+    # wait to accept a connection - blocking call
+    conn, addr = s.accept()
+    # print('Connected with ' + addr[0] + ':' + str(addr[1]))
+
+    data = conn.recv(2048)
+    newdata = data.decode('utf-8')
+    # decrypted = decrypt(newdata)
+    # print(decrypted)
+    print(newdata)
+
 
 s.close()
-
-
-
